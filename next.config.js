8b67bf63bd485c3cdfc13a6bced0cb03b4a52d64
duzgunberke <79326338+duzgunberke@users.noreply.github.com>
2022-03-24 08:53:49 +0300

    next.config update

--- next.config.js
+++ next.config.js
@@ -1,4 +1,13 @@
 const withPlugins = require('next-compose-plugins');
 const optimizedImages = require('next-optimized-images');
-
+const debug = process.env.NODE_ENV !== "production";
+module.exports = {
+  exportPathMap: function () {
+    return {
+      "/": { page: "/" },
+      "/ap-grid-layout": { page: "/ap-grid-layout" },
+    }
+  },
+  assetPrefix: !debug ? 'https://anotherplanet-io.github.io/Next-React-Components/' : '',
+}
 module.exports = withPlugins([optimizedImages], { target: 'serverless' });
